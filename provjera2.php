658f9c225975a4531e8c1be96fc563ac554c5864
be_dev <be_dev@Marijas-MacBook-Pro.local>
2026-03-26 18:43:39 +0100

    Add logic for STRING

--- provjera2.php
+++ provjera2.php
@@ -14,6 +14,34 @@
 
   $storagePath = __DIR__ . "/storage";
 
+  if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0777, true);
+  }
+
+  $podatak = $_GET["podatak"] ?? "";
+
+  echo "<h2>Primljeni podatak: {$podatak}</h2>";
+
+  // STRINGOVI
+  if (!is_numeric($podatak)) {
+
+    echo "<h3>Ovo je STRING.</h3>";
+
+    $_SESSION["stringovi"][] = $podatak;
+    $file = $storagePath . "/tekstovi.json";
+
+    if (!file_exists($file)) {
+      file_put_contents($file, json_encode([]));
+    }
+
+    $podaci = json_decode(file_get_contents($file), true);
+    $podaci[] = $podatak;
+
+    file_put_contents($file, json_encode($podaci, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+    echo "<label>Do sada je obrađeno " . count($podaci) . " stringova</label>";
+  }
+
   ?>
 
 </body>
